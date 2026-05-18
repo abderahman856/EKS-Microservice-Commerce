50090c8dc1b22f680d6425d922b74b1ea44ef35c
Baashe <baashe@Baashes-MacBook-Air.local>
2026-05-18 16:35:41 +0300

    fix(eks): correct invalid control plane logging type string

--- terraform/Modules/eks/main.tf
+++ terraform/Modules/eks/main.tf
@@ -2,7 +2,7 @@ resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = var.cluster_role_arn
 
-  enabled_cluster_log_types = ["api", "audit", "authenticator", "profile", "scheduler"]
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 
   vpc_config {
     subnet_ids = var.subnet_ids
